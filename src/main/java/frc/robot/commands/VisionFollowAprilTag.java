a38ec587783bf3f311820acf9eb657ee040d4975
TimebotsGithubno2 <githubtimebots52752@gmail.com>
2024-01-20 14:41:27 -0600

    Start new Vision Command

--- src/main/java/frc/robot/commands/VisionFollowAprilTag.java
+++ src/main/java/frc/robot/commands/VisionFollowAprilTag.java
@@ -4,23 +4,16 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.CustomTypes.Math.Vector2;
+import frc.robot.subsystems.DriveTrain.SwerveDrive;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class VisionFollowAprilTag extends Command {
+  
+  SwerveDrive swerveDrive;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public VisionFollowAprilTag(SwerveDrive swerveDrive) {
+    this.swerveDrive = swerveDrive;
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +22,10 @@ public class ExampleCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute()
+  {
+    
+  }
 
   // Called once the command ends or is interrupted.
   @Override
--- src/main/java/frc/robot/subsystems/Vision/VisionDriveCalculator.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionDriveCalculator.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.Vision;
 
 import frc.robot.CustomTypes.Math.SillyMath;
+import frc.robot.CustomTypes.Math.Vector2;
 
 public class VisionDriveCalculator {
     static Vision vision;
@@ -12,5 +13,12 @@ public class VisionDriveCalculator {
         final double rotSpeed = .5f;
 
         return -SillyMath.clamp(vision.HorizontalOffsetFromTarget() / 15, -1, 1) * rotSpeed;
+    }
+
+    public static Vector2 GetDirectionToAprilTagOffset(Vector2 aprilTagOffset)
+    {
+        Vector2 targetOffsetRelativeToRobot = new Vector2(vision.TargetPosInRobotSpace().x, vision.TargetPosInRobotSpace().z);
+
+        return Vector2.zero;
     }
 }
